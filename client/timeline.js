9fd27f265958b1e0f7176e9bb02f1d85618b73e0
Tyler Adams <tyleraadams@gmail.com>
2014-08-26 00:56:11 -0400

    added sound control functionality. tweaked color scheme. began constructor function to build templates

--- client/timeline.js
+++ client/timeline.js
@@ -9,12 +9,22 @@ PhotosFrameTemplate = (function(){
 	}
 
 	function onPhotoClick(e) {	
-		setSessions()	
+		setSessions();	
 		var name = e.target.id.charAt(0).toUpperCase() +  e.target.id.slice(1).split("-")[0];
 		Session.set(name+"On",true);
 		$($(e.target).siblings()).css('border-color','white');
 		$(e.target).css('border-color','#FFA18F');
 		$("body,html").animate({scrollTop: "850px"}, 1000);
+	}
+
+
+	function onAudioClick(e){
+		var audioPlayer = document.getElementById('player');
+		if(audioPlayer.paused){
+			audioPlayer.play();
+		}else{
+			audioPlayer.pause();
+		}
 	}
 
 	function onRender(){
@@ -45,7 +55,8 @@ PhotosFrameTemplate = (function(){
 		getMillerOn:getMillerOn,
 		getColemanOn:getColemanOn,
 		getEcholsOn: getEcholsOn,
-		getAboutOff:getAboutOff
+		getAboutOff:getAboutOff,
+		onAudioClick:onAudioClick
 	}
 })();
 
@@ -56,5 +67,103 @@ Template.photosFrame.getColemanOn = PhotosFrameTemplate.getColemanOn;
 Template.photosFrame.getEcholsOn = PhotosFrameTemplate.getEcholsOn;
 Template.photosFrame.getAboutOff = PhotosFrameTemplate.getAboutOff;
 Template.photosFrame.events = {
-	"click .photo": PhotosFrameTemplate.onPhotoClick
+	"click .photo": PhotosFrameTemplate.onPhotoClick,
+	"click #audioButton": PhotosFrameTemplate.onAudioClick
 }
+
+// TemplateManager = (function)(){
+
+// 	function TemplateConstructor(name) {
+// 		this.name = name + "Template";
+
+// 		this.onRender = function(){
+			
+// 		}
+		
+// 	};
+
+// 	function createWomenTemplates(){
+
+
+// 	}
+
+
+// })();
+
+MillerTemplate = (function(){
+
+	function onRender(){
+		createAudioOffListener()
+	}
+	
+	function createAudioOffListener(){
+	var audioPlayer = document.getElementById('player');
+		audioPlayer.addEventListener('ended', disappearAudioButton);
+	}
+
+	function disappearAudioButton() {
+		console.log("the audio is now off, now dim the volume button");
+		$('#audioButton').css('display','none');
+	}
+
+	return {
+		onRender:onRender
+	}
+})();
+
+Template.miller.rendered = MillerTemplate.onRender;
+
+
+
+ColemanTemplate = (function(){
+
+	function onRender(){
+		createAudioOffListener()
+	}
+	
+	function createAudioOffListener(){
+	var audioPlayer = document.getElementById('player');
+		audioPlayer.addEventListener('ended', disappearAudioButton);
+	}
+
+	function disappearAudioButton() {
+		console.log("the audio is now off, now dim the volume button");
+		$('#audioButton').css('display','none')
+	}
+
+	return {
+		onRender:onRender
+	}
+})();
+
+Template.coleman.rendered = ColemanTemplate.onRender;
+
+
+EcholsTemplate = (function(){
+
+	function onRender(){
+		createAudioOffListener()
+	}
+	
+	function createAudioOffListener(){
+	var audioPlayer = document.getElementById('player');
+		audioPlayer.addEventListener('ended', disappearAudioButton);
+	}
+
+	function disappearAudioButton() {
+		console.log("the audio is now off, now dim the volume button");
+		$('#audioButton').css('display','none')
+	}
+
+	return {
+		onRender:onRender
+	}
+})();
+
+Template.echols.rendered = EcholsTemplate.onRender;
+
+
+
+
+
+
