46d6394b218acc6c4dc466a53fb36a7f28f8ff86
Tyler Adams <tyleraadams@gmail.com>
2014-09-05 01:16:54 -0400

    added funtion to incorporate google analytics

--- client/timeline.js
+++ client/timeline.js
@@ -30,6 +30,27 @@ PhotosFrameTemplate = (function(){
 	function onRender(){
 		setSessions();
 		getAboutOff();
+		setUpAnalytics();
+	}
+
+	function setUpAnalytics(){
+
+		if (!(window._gaq != null)) {
+		    window._gaq = [];
+		    _gaq.push(['_setAccount', 'UA-54472436-1']);
+		    _gaq.push(['_trackPageview']);
+		    return (function() {
+				var ga, gajs, s;
+				ga = document.createElement('script');
+				ga.type = 'text/javascript';
+				ga.async = true;
+				gajs = '.google-analytics.com/ga.js';
+				ga.src = 'https:' === document.location.protocol ? 'https://ssl' + gajs : 'http://www' + gajs;
+				s = document.getElementsByTagName('script')[0];
+				return s.parentNode.insertBefore(ga, s);
+		    });
+		}
+
 	}
 
 	function getMillerOn(){
